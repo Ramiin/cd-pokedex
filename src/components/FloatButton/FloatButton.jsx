8aae377eb4ee430df72dba9760b0589f0f20ba4a
Oscar Rdz <raadgtooz@gmail.com>
2026-04-15 19:23:09 -0600

    refactor: rewrite FloatButton as accessible react-router Link

---
+++ src/components/FloatButton/FloatButton.jsx
@@ -0,0 +1,9 @@
+import { Link } from 'react-router-dom';
+
+export default function FloatButton() {
+  return (
+    <div className="float-container">
+      <Link to="/about">About</Link>
+    </div>
+  );
+}
